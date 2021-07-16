848ed7517de9dc59eea1f7a6bd8b3065b83ee691
CsYBot <67687653+CsYBot@users.noreply.github.com>
2021-07-16 17:51:11 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ commandcreate("sa", "<@-userid->, As Hoşgeldin Bro!");
 
 csyhost.ready("Hello CsYHosted", "csycraft");
 
-csyhost.login('o3b3w334r3456323o39344u2x476z4z2o3c4u3r2i3i4o4p214z3z4g3g303r4z3b3o5n5u4v3z55523k4k3o2z5z2l573z2e3p3j4p4q4o4u5k3r4c3x2l3r3p3t4b3t4h3t4h3n3j3i3o4b3s4q4u4o3l4r4h3f3e3h3m4j3r4f3h3f3c3u2w25474b41303r2z2v2y2x284s254u264w2 ');
+csyhost.login('o3b3w334r3456323o39344u2x476z4z2o3c4u3r2i3i4o4p214z3z4g3g303r4z3b3o5n5u4v3z55523k4k3o2z5z2l573z2e3p3j4p4q4o4u5k3r4c3x2l3r3p3t4b3t4h3t4h3n3j3i3o4b3s4q4u4o3l4r4h3f3e3h3m4j3r4f3h3f3c3u2w25474b41303r2z2v2y2x284s254u264w2');
