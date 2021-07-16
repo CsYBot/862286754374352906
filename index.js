bb71aad7bc01df0c54eb76824810aa48a2a0edc5
CsYBot <67687653+CsYBot@users.noreply.github.com>
2021-07-16 11:28:13 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+commandcreate("hi", "hello -username-");
+
+csyhost.ready("Hello CsYHosted", "csycraft");
+
+csyhost.owner('542255512591532032');
+csyhost.login('o3b3w334r3456323o39344u2x476z4z2o3c4u3r2i3i4o4p214z3z4g3g303r4z3b3o5n5u4v3z55523k4k3o2z5z2l573z2e3p3j4p4q4o4u5k3r4c3x2');
